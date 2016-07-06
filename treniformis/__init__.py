4981e9f48885619c793affdc1eef01ac6a4229f5
Kevin Wurster <kevin@skytruth.org>
2016-07-06 14:50:31 -0400

    Quick code cleanup

--- treniformis/__init__.py
+++ treniformis/__init__.py
@@ -20,7 +20,7 @@ is available for them using `help(obj)` or `print(obj.__doc__)`:
 
 * known_likely_fishing: combination of known and likely MMSI lists
 
-	
+    
 * spoofing: MMSI that appear to be associated with spoofing.
 
 
@@ -35,80 +35,81 @@ clasification_lists_path: path to directory of miscelaneous classification lists
 In addition to the stuff documented above there are many items
 installed in `internal` that are primarily used as the sources
 for the publised lists. Or are old and are kept around for reference
-
-
 """
 
+
+import glob
+import os
+
+
 def _path(*subpaths):
-	import os
-	here = os.path.dirname(__file__)
-	return os.path.join(here, *subpaths)
-
-def _load_mmsis_by_year(name, path, year_index, 
-						pattern="*.txt",
-						readme="README.md"):
-	import glob, os
-	if readme:
-		doc = open(_path(path, readme)).read()
-	else:
-		doc = None
-	MmsiMap = type(name, (dict,), {"__doc__": doc})
-	paths = glob.glob(_path(path, pattern))
-	mmsi_map = MmsiMap()
-	for p in paths:
-		name, _ = os.path.splitext(os.path.basename(p))
-		year = int(name.split('-')[year_index])
-		mmsi = [x.strip() for x in open(p).read().strip().split('\n')]
-		mmsi_map[year] = mmsi
-	return mmsi_map
+    here = os.path.dirname(__file__)
+    return os.path.join(here, *subpaths)
 
 
-def _load_list(name, path, pattern="*.txt", readme=None):
-	import glob, os
-	if readme:
-		doc = open(_path(path, readme)).read()
-	else:
-		doc = None
-	ListLike = type(name, (list,), {"__doc__": doc})
-	full_path = _path(path, name)
-	lines = open(full_path).read().strip().split('\n')
-	return ListLike([x.strip() for x in lines])
+def _load_mmsis_by_year(
+        name, path, year_index, pattern="*.txt", readme="README.md"):
+
+    if readme:
+        with open(_path(path, readme)) as f:
+            doc = f.read()
+    else:
+        doc = None
+    mmsi_map = type(name, (dict,), {"__doc__": doc})
+    paths = glob.glob(_path(path, pattern))
+    mmsi_map = mmsi_map()
+    for p in paths:
+        name, _ = os.path.splitext(os.path.basename(p))
+        year = int(name.split('-')[year_index])
+        mmsi = [x.strip() for x in open(p).read().strip().split('\n')]
+        mmsi_map[year] = mmsi
+    return mmsi_map
+
+
+def _load_list(name, path, readme=None):
+    if readme:
+        with open(_path(path, readme)) as f:
+            doc = f.read()
+    else:
+        doc = None
+    list_like = type(name, (list,), {"__doc__": doc})
+    full_path = _path(path, name)
+    with open(full_path).read() as f:
+        lines = f.splitlines()
+    return list_like([x.strip() for x in lines])
 
 
 active_mmsis = _load_mmsis_by_year(
-					name="active_mmsis", 
-					path="exported/active-mmsis/active-mmsis-v1",
-					year_index=2)
+    name="active_mmsis",
+    path="exported/active-mmsis/active-mmsis-v1",
+    year_index=2)
 
 known_fishing = _load_mmsis_by_year(
-					name="known_fishing", 
-					path="known-fishing/known-fishing-v1",
-					year_index=2)
+    name="known_fishing",
+    path="known-fishing/known-fishing-v1",
+    year_index=2)
 
 likely_fishing = _load_mmsis_by_year(
-					name="likely_fishing", 
-					path="exported/likely-fishing/likely-fishing-v2",
-					year_index=2)
+    name="likely_fishing",
+    path="exported/likely-fishing/likely-fishing-v2",
+    year_index=2)
 
 spoofing = _load_mmsis_by_year(
-					name="spoofing", 
-					path="exported/spoofing/spoofing-mmsis-v3",
-					year_index=2)
+    name="spoofing",
+    path="exported/spoofing/spoofing-mmsis-v3",
+    year_index=2)
 
 known_likely_fishing = _load_mmsis_by_year(
-					name="known_likely_fishing", 
-					path="published",
-					year_index=4,
-					pattern="known-likely-fishing-mmsis-*.txt",
-					readme=None)
+    name="known_likely_fishing",
+    path="published",
+    year_index=4,
+    pattern="known-likely-fishing-mmsis-*.txt",
+    readme=None)
 
 
-vessel_classes = _load_list(name="vessel_classes.md",
-						    path="",
-						    readme=None)
+vessel_classes = _load_list(
+    name="vessel_classes.md",
+    path="",
+    readme=None)
 
 classification_lists_path = _path("internal/classification_lists")
-
-
-
-
