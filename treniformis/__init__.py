8abf6271c192b1c00ec61a65a560a6e95f422ccb
Kevin Wurster <kevin@skytruth.org>
2016-07-06 16:24:48 -0400

    MVP API

--- treniformis/__init__.py
+++ treniformis/__init__.py
@@ -38,25 +38,26 @@ for the publised lists. Or are old and are kept around for reference
 """
 
 
-import os
+from treniformis._mvp_api import get_annual_list
 
 
-def get_annual_list(asset_id):
-    """Get a path to an MMSI list, which is probably a text file with a single
-    MMSI per line.
+__version__ = '0.0.1'
+__author__ = 'Global Fishing Watch'
+__email__ = 'info@globalfishingwatch.org'
+__source__ = 'https://github.com/GlobalFishingWatch/treniformis'
+__license__ = """
+Copyright 2016 SkyTruth
+Authors: Global Fishing Watch
 
-    This is an MVP API that will almost certainly be deprecated at some point.
-    Do not construct file paths directly.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    Parameters
-    ----------
-    asset_id : str
-        Like ``GFW/WKV/KNOWN_FISHING/2014``.
+   http://www.apache.org/licenses/LICENSE-2.0
 
-    Returns
-    -------
-    str
-        File path.
-    """
-    return os.path.join(
-        os.path.dirname(__file__), '_assets', '{}.txt'.format(asset_id))
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
---
+++ treniformis/_mvp_api.py
@@ -0,0 +1,33 @@
+"""MVP API"""
+
+
+import os
+
+from treniformis import errors
+
+
+def get_annual_list(asset_id):
+    """Get a path to an MMSI list, which is probably a text file with a single
+    MMSI per line.
+
+    This is an MVP API that will almost certainly be deprecated at some point.
+    Do not construct file paths directly.
+
+    Parameters
+    ----------
+    asset_id : str
+        Like ``GFW/WKV/KNOWN_FISHING/2014``.
+
+    Returns
+    -------
+    str
+        File path.
+    """
+
+    path = os.path.join(
+        os.path.dirname(__file__), '_assets', '{}.txt'.format(asset_id))
+    if not os.path.exists(path) or not os.path.isfile(path):
+        raise errors.TreniformisIOError(
+            "Invalid asset ID: {}".format(asset_id))
+
+    return path
