f5b8713be46547b28e305e5f58a812a92ab612c0
bitsofbits <tim@bitsofbits.com>
2016-10-13 18:00:47 -0700

    update table names; change logistic_table_name to classify_table_name

--- scripts/sql/spoofing-mmsis-v3.sql
+++ scripts/sql/spoofing-mmsis-v3.sql
@@ -24,7 +24,7 @@ FROM (
       COUNT(*) message_count,
       MIN(TIMESTAMP_TO_SEC(timestamp)) AS min_timestamp,
       MAX(TIMESTAMP_TO_SEC(timestamp)) AS max_timestamp
-    FROM (TABLE_DATE_RANGE([{logistic_table_name}.], TIMESTAMP('{start_date}'), TIMESTAMP('{end_date}')))
+    FROM (TABLE_DATE_RANGE([{classify_table_name}.], TIMESTAMP('{start_date}'), TIMESTAMP('{end_date}')))
     WHERE
       RIGHT(seg_id, 3) != 'BAD'
     GROUP BY
@@ -43,7 +43,7 @@ JOIN (
       COUNT(*) AS message_count,
       MIN(TIMESTAMP_TO_SEC(timestamp)) AS min_timestamp,
       MAX(TIMESTAMP_TO_SEC(timestamp)) AS max_timestamp
-    FROM (TABLE_DATE_RANGE([{logistic_table_name}.], TIMESTAMP('{start_date}'), TIMESTAMP('{end_date}')))
+    FROM (TABLE_DATE_RANGE([{classify_table_name}.], TIMESTAMP('{start_date}'), TIMESTAMP('{end_date}')))
     WHERE
       RIGHT(seg_id, 3) != 'BAD'
     GROUP BY
