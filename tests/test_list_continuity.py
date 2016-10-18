9cdca9f876276a1de64a315f0fba3511c3c4be52
bitsofbits <tim@bitsofbits.com>
2016-10-18 09:44:13 -0700

    sort REEFERS and update tests

--- tests/test_list_continuity.py
+++ tests/test_list_continuity.py
@@ -5,6 +5,7 @@ they remain sorted and unique.
 
 
 import os
+import csv
 
 import pytest
 
@@ -17,6 +18,18 @@ def _get_mmsi_lists():
     for root, dirs, files in os.walk(base):
         for name in files:
             if os.path.splitext(name)[1].lower() == '.txt':
+                yield os.path.join(root, name)
+
+
+# Files that should not be included in _get_csv_lists
+CSV_BLACKLIST = set()
+
+def _get_csv_lists():
+    "Yields paths to files containing CSVs with a column named `mmsi`"
+    base = os.path.join(os.path.dirname(treniformis.__file__), '_assets')
+    for root, dirs, files in os.walk(base):
+        for name in files:
+            if name not in CSV_BLACKLIST and os.path.splitext(name)[1].lower() == '.csv':
                 yield os.path.join(root, name)
 
 
@@ -51,3 +64,30 @@ def test_mmsi_list_unique(path):
     with open(path) as f:
         stripped = [l.strip() for l in f]
         assert len(stripped) == len(set(stripped))
+
+def _mmsi_reader(f):
+    reader = csv.DictReader(f)
+    for x in reader:
+        yield x['mmsi'].strip()
+
+@pytest.mark.parametrize("path", _get_csv_lists())
+def test_csv_list_sorted(path):
+    """CSV lists should remain sorted by MMSI."""
+    with open(path) as f:
+        assert is_sorted(_mmsi_reader(f))
+
+
+@pytest.mark.parametrize("path", _get_mmsi_lists())
+def test_mmsi_list_unique(path):
+    """MMSI lists should not contain duplicates."""
+    with open(path) as f:
+        stripped = [l.strip() for l in f]
+        assert len(stripped) == len(set(stripped))
+
+
+@pytest.mark.parametrize("path", _get_csv_lists())
+def test_csv_list_unique(path):
+    """CSV lists should not contain duplicate MMSI."""
+    with open(path) as f:
+        mmsi = list(_mmsi_reader(f))
+        assert len(mmsi) == len(set(mmsi))
