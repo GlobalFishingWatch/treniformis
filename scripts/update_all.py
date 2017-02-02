51c257d9a6be3f07a7e47b4bbd14340a4fac2cc7
bitsofbits <tim@bitsofbits.com>
2017-02-02 12:14:22 -0700

    everything automated except BQ uploads

--- scripts/update_all.py
+++ scripts/update_all.py
@@ -3,7 +3,10 @@ from update_filter_lists import update_derived_lists
 from update_readmes import update_readmes
 
 
-if __name__ == "__main__":
+def update_all():
     update_base_lists()
     update_derived_lists()
     update_readmes()
+
+if __name__ == "__main__":
+    update_all()
